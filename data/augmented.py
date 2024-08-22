8af8fab058fd93bb23fd1e6473bfefedcbea217c
lama-hisham <lamahishamnouh@gmail.com>
2024-08-22 04:22:34 +0300

    Delete data directory

--- data/augmented.py
+++
@@ -1,8 +0,0 @@
-import torchvision.transforms as transforms
-
-def get_augmented_transform():
-    transform = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(10)
-    ])
-    return transform
--- data/split.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def split_data(dataset, train_size, val_size, test_size):
-    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
-    return train_dataset, val_dataset, test_dataset
